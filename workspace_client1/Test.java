3a8e0268c6ede391258e85f384b8a7886415be89
Afonso <afonsodev@outlook.com>
2024-04-18 10:35:13 +0100

    - fixes

---
+++ workspace_client1/Test.java
@@ -0,0 +1,17 @@
+//9e30e98a-36db-47f4-836c-16c390a1d2d7
+package test;
+
+//13c9f311-0d07-46aa-8591-ef22c6ab8e49
+class Test {
+
+    //ce2411ab-72e7-4c2b-b4b1-3e20dc187945
+    int metad(String b) {
+        int test = 5;
+    }
+
+    //d0779f95-d537-4501-b708-fc50747e6616
+    String method(String parama) {
+        param = param + 6;
+    }
+}
+
---
+++ workspace_client1/root/Test.java
@@ -0,0 +1,21 @@
+//9e30e98a-36db-47f4-836c-16c390a1d2d7
+package test;
+
+//13c9f311-0d07-46aa-8591-ef22c6ab8e49
+class Test {
+
+    //e2462642-4ad1-4873-a4b4-1d66eb194352
+    String test() {
+        ;
+    }
+
+    //ce2411ab-72e7-4c2b-b4b1-3e20dc187945
+    int metad(String b) {
+        int test = 5;
+    }
+
+    //d0779f95-d537-4501-b708-fc50747e6616
+    String method(String parama) {
+        param = param + 6;
+    }
+}
